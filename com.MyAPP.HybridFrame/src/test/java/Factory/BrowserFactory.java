e06e8e139a2e112982993b91258688dfd113544c
AfsanaTopa <afsanakhantopa333@gmail.com>
2020-02-23 19:35:38 -0500

    chrome option package

--- com.MyAPP.HybridFrame/src/test/java/Factory/BrowserFactory.java
+++ com.MyAPP.HybridFrame/src/test/java/Factory/BrowserFactory.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import DataProvider.ConfigurationDataProvider;
 
